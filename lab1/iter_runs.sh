d5c9a866104e39a2fca9954a7ecaffdcb4051ae0
Jayesh Joshi <jjoshi@login3.stampede2.tacc.utexas.edu>
2021-02-07 11:26:34 -0600

    Add support for cache aware

--- lab1/iter_runs.sh
+++ lab1/iter_runs.sh
@@ -1,8 +1,14 @@
 #!/bin/bash
 
 NUM_RUNS=5
-TARGET=('basicmatmul')
+TARGET=('basicmatmul' 'cacheaware')
+CACHEAWARE='cacheaware'
 N=(32 512 4096)  # Assuming square matrices
+
+# These are NOT calculated properly yet...
+B1=128
+B2=8192
+B3=16384
 
 BASICPERF='cycles:u,instructions:u,cache-references:u,cache-misses:u'
 L1PERF='L1-dcache-load:u,L1-dcache-load-misses:u,L1-dcache-stores:u,L1-dcache-store-misses:u'
@@ -10,13 +16,27 @@ LLCPERF='LLC-loads:u,LLC-load-misses:u,LLC-stores:u,LLC-store-misses:u'
 
 for target in "${TARGET[@]}"; do
     for side in "${N[@]}"; do
+        b1_tgt=$((B1 < side ? B1 : side))
+        b2_tgt=$((B2 < side ? B2 : side))
+        b3_tgt=$((B3 < side ? B3 : side))
+
+        if [[ "$target" == "$CACHEAWARE" ]]; then
+            b1_arg="$b1_tgt"
+            b2_arg="$b2_tgt"
+            b3_arg="$b3_tgt"
+        else
+            b1_arg=""
+            b2_arg=""
+            b3_arg=""
+        fi
+
         TARGET_DIR="results/${target}_$side-$side-$side/"
         mkdir -p $TARGET_DIR
         for ((run = 1; run <= $NUM_RUNS; run++))
         do
-            echo "perf stat -o $TARGET_DIR/basic_perf_run${run}.out -e $BASICPERF ./$target $side $side $side" 
-            echo "perf stat -o $TARGET_DIR/l1_perf_run${run}.out -e $L1PERF ./$target $side $side $side"
-            echo "perf stat -o $TARGET_DIR/llc_perf_run${run}.out -e $LLCPERF ./$target $side $side $side" 
+            echo "perf stat -o $TARGET_DIR/basic_perf_run${run}.out -e $BASICPERF ./$target $side $side $side $b1_arg $b2_arg $b3_arg" 
+            echo "perf stat -o $TARGET_DIR/l1_perf_run${run}.out -e $L1PERF ./$target $side $side $side $b1_arg $b2_arg $b3_arg"
+            echo "perf stat -o $TARGET_DIR/llc_perf_run${run}.out -e $LLCPERF ./$target $side $side $side $b1_arg $b2_arg $b3_arg" 
         done
     done
 done
--- lab1/src/basic_matmul.c
+++ lab1/src/basic_matmul.c
@@ -8,23 +8,23 @@ extern int N, M, P;
 
 // calculate C = AxB
 void matmul(float **A, float **B, float **C){
-  float sum;
-  int   i;
-  int   j;
-  int   k;
+    float sum;
+    int     i;
+    int     j;
+    int     k;
 
-  for (i=0; i<M; i++) {
-    // for each row of C
-    for (j=0; j<N; j++) {
-      // for each column of C
-      sum = 0.0f; // temporary value
-      for (k=0; k<P; k++) {
-        // dot product of row from A and column from B
-        sum += A[i][k]*B[k][j];
-      }
-      C[i][j] = sum;
+    for (i=0; i<M; i++) {
+        // for each row of C
+        for (j=0; j<N; j++) {
+            // for each column of C
+            sum = 0.0f; // temporary value
+            for (k=0; k<P; k++) {
+                // dot product of row from A and column from B
+                sum += A[i][k]*B[k][j];
+            }
+            C[i][j] = sum;
+        }
     }
-  }
 }
 
 // function to allocate a matrix on the heap
@@ -32,33 +32,33 @@ void matmul(float **A, float **B, float **C){
 //
 // the matrices are in row-major order.
 void create_matrix(float*** A, int m, int n) {
-  float **T = 0;
-  int i;
+    float **T = 0;
+    int i;
 
-  T = (float**)malloc( m*sizeof(float*));
-  for ( i=0; i<m; i++ ) {
-     T[i] = (float*)malloc(n*sizeof(float));
-  }
-  *A = T;
+    T = (float**)malloc( m*sizeof(float*));
+    for ( i=0; i<m; i++ ) {
+         T[i] = (float*)malloc(n*sizeof(float));
+    }
+    *A = T;
 }
 
 int main(int argc, char *argv[]) {
-  float** A;
-  float** B;
-  float** C;
+    float** A;
+    float** B;
+    float** C;
 
-  get_args(NUM_ADDL_ARGS, "", argc, argv);
+    get_args(NUM_ADDL_ARGS, "", argc, argv);
 
-  create_matrix(&A, M, P);
-  create_matrix(&B, P, N);
-  create_matrix(&C, M, N);
+    create_matrix(&A, M, P);
+    create_matrix(&B, P, N);
+    create_matrix(&C, M, N);
 
-  // assume some initialization of A and B
-  // think of this as a library where A and B are
-  // inputs in row-major format, and C is an output
-  // in row-major.
+    // assume some initialization of A and B
+    // think of this as a library where A and B are
+    // inputs in row-major format, and C is an output
+    // in row-major.
 
-  matmul(A, B, C);
+    matmul(A, B, C);
 
-  return (0);
+    return (0);
 }
